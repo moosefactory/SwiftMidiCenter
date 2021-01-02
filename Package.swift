0f6e8a865e6b6828ad82190392bd6a409eab74cf
Tristan Leblanc <tristan@moosefactory.eu>
2021-01-03 00:55:55 +0100

    Added swift package product depency

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftMidiCenter",
-            dependencies: []),
+            dependencies: ["SwiftMIDI"]),
         .testTarget(
             name: "SwiftMidiCenterTests",
             dependencies: ["SwiftMidiCenter"]),
