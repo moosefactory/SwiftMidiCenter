2c1e381387ff1f2a8c417953976aa8ab44dda5da
Tristan <t.leblanc@externe.liberation.fr>
2022-11-19 21:38:42 +0100

    v1.0.6 - upToNextMajor

--- Package.swift
+++ Package.swift
@@ -17,9 +17,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/moosefactory/SwiftMIDI", from: "1.0.5"),
-        .package(url: "https://github.com/moosefactory/MoofFoundation", from: "1.1.0"),
-        .package(url: "https://github.com/moosefactory/UniColor", from: "1.0.2"),
+        .package(url: "https://github.com/moosefactory/SwiftMIDI.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/moosefactory/MoofFoundation.git", .upToNextMajor(from: "1.1.0")),
+        .package(url: "https://github.com/moosefactory/UniColor.git", .upToNextMajor(from: "1.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
