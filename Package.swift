d161a6dfa24023f0a242b2a90e4643b9ef8d82b9
Tristan <t.leblanc@externe.liberation.fr>
2022-11-19 21:29:41 +0100

    Stop Package.resolved Tracking - Remote frameworks

--- Package.swift
+++ Package.swift
@@ -17,9 +17,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "../SwiftMIDI", from: "1.0.5"),
-        .package(url: "../MoofFoundation", from: "1.1.0"),
-        .package(url: "../UniColor", from: "1.0.0"),
+        .package(url: "https://github.com/moosefactory/SwiftMIDI", from: "1.0.5"),
+        .package(url: "https://github.com/moosefactory/MoofFoundation", from: "1.1.0"),
+        .package(url: "https://github.com/moosefactory/UniColor", from: "1.0.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -29,6 +29,6 @@ let package = Package(
             dependencies: ["SwiftMIDI", "MoofFoundation", "UniColor"]),
         .testTarget(
             name: "SwiftMidiCenterTests",
-            dependencies: ["SwiftMidiCenter", "MoofFoundation", "UniColor"]),
+            dependencies: ["SwiftMidiCenter"]),
     ]
 )
